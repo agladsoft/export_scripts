a17fa3f167d29ddf22fea6a294bda4267b96c2ca
uvens <uventus1992@gmail.com>
2023-09-29 15:53:57 +0300

    add parsed line msc and arkas

--- scripts/parsed.py
+++ scripts/parsed.py
@@ -62,7 +62,7 @@ EXPORT = ['export', 'экспорт']
 class ParsedDf:
     def __init__(self, df):
         self.df = df
-        self.url = "service_consignment:8004"
+        self.url = "http://service_consignment:8004"
         self.headers = {
             'Content-Type': 'application/json'
         }
