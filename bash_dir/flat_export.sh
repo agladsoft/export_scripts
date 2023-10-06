33afccd464d3dbd77bc55d6b47c78217ac61ca8a
timur <timurka.zavyalov@mail.ru>
2023-10-06 16:57:46 +0300

    add: added new names columns

--- bash_dir/flat_export.sh
+++ bash_dir/flat_export.sh
@@ -1,45 +1,36 @@
 #!/bin/bash
 
-# Declare a string array with type
-declare -a DirsArray=(
-    "flat_export_nutep_tracking"
-    "flat_export_nutep_tracking_update"
-)
+xls_path="${XL_IDP_PATH_EXPORT}/lines_${XL_IMPORT_TERMINAL}/flat_export_${XL_IMPORT_TERMINAL}"
 
-for dir in "${DirsArray[@]}"; do
+done_path="${xls_path}"/done
+if [ ! -d "$done_path" ]; then
+  mkdir "${done_path}"
+fi
 
-    xls_path="${XL_IDP_PATH_EXPORT}/lines_${XL_IMPORT_TERMINAL}/${dir}"
+json_path="${xls_path}"/json
+if [ ! -d "$json_path" ]; then
+  mkdir "${json_path}"
+fi
 
-    done_path="${xls_path}"/done
-    if [ ! -d "$done_path" ]; then
-      mkdir "${done_path}"
-    fi
+find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" -or -name "*.xml" \) ! -newermt '3 seconds ago' -print0 | while read -d $'\0' file
+do
 
-    json_path="${xls_path}"/json
-    if [ ! -d "$json_path" ]; then
-      mkdir "${json_path}"
-    fi
+  if [[ "${file}" == *"error_"* ]];
+  then
+    continue
+  fi
 
-    find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" -or -name "*.xml" \) ! -newermt '3 seconds ago' -print0 | while read -d $'\0' file
-    do
+	mime_type=$(file -b --mime-type "$file")
+  echo "'${file} - ${mime_type}'"
 
-      if [[ "${file}" == *"error_"* ]];
-      then
-        continue
-      fi
+	# Will convert csv to json
+	python3 ${XL_IDP_ROOT_EXPORT}/scripts/flat_export.py "${file}" "${json_path}"
 
-        mime_type=$(file -b --mime-type "$file")
-      echo "'${file} - ${mime_type}'"
+  if [ $? -eq 0 ]
+	then
+	  mv "${file}" "${done_path}"
+	else
+	  mv "${file}" "${xls_path}/error_$(basename "${file}")"
+	fi
 
-        # Will convert csv to json
-        python3 ${XL_IDP_ROOT_EXPORT}/scripts/flat_export.py "${file}" "${json_path}"
-
-      if [ $? -eq 0 ]
-        then
-          mv "${file}" "${done_path}"
-        else
-          mv "${file}" "${xls_path}/error_$(basename "${file}")"
-        fi
-
-    done
 done
