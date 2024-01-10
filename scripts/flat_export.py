6517855a19cd420ccc0a8d3bf8495e6d69fddb3b
uvens <uventus1992@gmail.com>
2024-01-10 16:07:34 +0300

    add telegram notification

--- scripts/flat_export.py
+++ scripts/flat_export.py
@@ -2,6 +2,7 @@ import re
 import os
 import sys
 import json
+import requests
 import contextlib
 import numpy as np
 import pandas as pd
@@ -13,6 +14,9 @@ from notifiers import get_notifier
 
 TOKEN = '6557326533:AAHy6ls9LhTVTGztix8PUSK7BUSaHVEojXc'
 CHAT_ID = '-1002064780308'
+TOPIC = '1069'
+ID = '1071'
+
 headers_eng: dict = {
     "Терминал": "terminal",
     "Линия": "line",
@@ -48,9 +52,17 @@ date_formats: tuple = ("%Y-%m-%d", "%d.%m.%Y", "%Y-%m-%d %H:%M:%S")
 
 
 def telegram(message):
-    teg = get_notifier('telegram')
-    teg.notify(token=TOKEN, chat_id=CHAT_ID, message=message)
-
+    # teg = get_notifier('telegram')
+    # teg.notify(token=TOKEN, chat_id=CHAT_ID, message=message)
+    chat_id = CHAT_ID
+    token = TOKEN
+    topic = TOPIC
+    message_id = ID
+    # teg.notify(token=get_my_env_var('TOKEN'), chat_id=get_my_env_var('CHAT_ID'), message=message)
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {"chat_id": f"{chat_id}/{topic}", "text": message,
+              'reply_to_message_id': message_id}  # Добавляем /2 для указания второго подканала
+    response = requests.get(url, params=params)
 
 class Export(object):
     def __init__(self, input_file_path: str, output_folder: str):
