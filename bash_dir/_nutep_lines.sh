8b48e391bb7b2d7dd228251948068cd03b5fdfe3
uvens <uventus1992@gmail.com>
2024-03-13 13:47:45 +0300

    add: flat_update_report_orders

--- bash_dir/_nutep_lines.sh
+++ bash_dir/_nutep_lines.sh
@@ -3,6 +3,6 @@ do
 	${XL_IDP_ROOT_EXPORT}/bash_dir/flat_export.sh;
 	${XL_IDP_ROOT_EXPORT}/bash_dir/export_grain.sh;
 	${XL_IDP_ROOT_EXPORT}/bash_dir/report_order.sh;
-	${XL_IDP_ROOT_EXPORT}/bash_dir/report_order_update.sh;
+	${XL_IDP_ROOT_EXPORT}/bash_dir/report_orders_update.sh;
 	sleep 1;
 done
