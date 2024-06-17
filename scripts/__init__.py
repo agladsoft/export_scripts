b639e17ff00b5bb344accbe900a5a11860faa29e
uventus <uventus_work@mail.ru>
2024-06-17 16:03:30 +0300

    fixed : change logic update data in database

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,19 +1,30 @@
+import os
+import requests
+from dotenv import load_dotenv
 from datetime import datetime, date
 
-TOKEN = '6557326533:AAHy6ls9LhTVTGztix8PUSK7BUSaHVEojXc'
-CHAT_ID = '-1002064780308'
-TOPIC = '1069'
-ID = '1071'
+
+load_dotenv()
+
+
+def get_my_env_var(var_name: str) -> str:
+    try:
+        return os.environ[var_name]
+    except KeyError as e:
+        raise MissingEnvironmentVariable(f"{var_name} does not exist") from e
+
+
+class MissingEnvironmentVariable(Exception):
+    pass
 
 
 def telegram(message):
     # teg = get_notifier('telegram')
     # teg.notify(token=TOKEN, chat_id=CHAT_ID, message=message)
-    chat_id = CHAT_ID
-    token = TOKEN
-    topic = TOPIC
-    message_id = ID
-    # teg.notify(token=get_my_env_var('TOKEN'), chat_id=get_my_env_var('CHAT_ID'), message=message)
+    chat_id = get_my_env_var('CHAT_ID')
+    token = get_my_env_var('TOKEN')
+    topic = get_my_env_var('TOPIC')
+    message_id = get_my_env_var('ID')
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     params = {"chat_id": f"{chat_id}/{topic}", "text": message,
               'reply_to_message_id': message_id}  # Добавляем /2 для указания второго подканала
